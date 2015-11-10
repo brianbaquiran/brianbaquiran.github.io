02fabd66539df51c62ccc9a73537ccebbafd3b4f
brianbaquiran <brianbaquiran@gmail.com>
2015-11-10 17:51:23 +0800

    Set correct homepage

--- conf.py
+++ conf.py
@@ -21,7 +21,7 @@ BLOG_AUTHOR = "Brian Baquiran"  # (translatable)
 BLOG_TITLE = "Brian Baquiran"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://example.com/"
+SITE_URL = "http://brianbaquiran@github.com/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://example.com/"
