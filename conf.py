b3f531435533b51fb79f13f543ca30dbec3f6448
brianbaquiran <brianbaquiran@gmail.com>
2015-12-04 13:49:56 +0800

    Add Google Analytics

--- conf.py
+++ conf.py
@@ -1059,6 +1059,16 @@ MARKDOWN_EXTENSIONS = ['fenced_code', 'codehilite', 'extra']
 # in the default template (base.tmpl).
 # (translatable)
 # BODY_END = ""
+BODY_END = """<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-10693543-2', 'auto');
+  ga('send', 'pageview');
+
+</script>"""
 
 # The possibility to extract metadata from the filename by using a
 # regular expression.
