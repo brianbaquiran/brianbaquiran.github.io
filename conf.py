cebc11b41a63b79d447a905ddcbff9ba21052240
Brian Baquiran <brianbaquiran@gmail.com>
2020-04-05 18:44:00 +0900

    Make site home https

--- conf.py
+++ conf.py
@@ -21,7 +21,7 @@ BLOG_AUTHOR = "Brian Baquiran"  # (translatable)
 BLOG_TITLE = "Lateral Thunking"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "http://brianbaquiran.github.io/"
+SITE_URL = "https://brianbaquiran.github.io/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://example.com/"
